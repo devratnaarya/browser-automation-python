277808acdb64c82b8611c337240ddfd82c1325ea
Devratna Arya <adevratna@gmail.com>
2019-05-06 15:01:30 +0530

    Update flipcart_auto_order.py

--- flipcart_auto_order.py
+++ flipcart_auto_order.py
@@ -12,10 +12,10 @@ driver.maximize_window()
 
 # wait for Fastrack menu item to appear, then click it
 loginId = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, "//*[@class='_2zrpKA']")));
-loginId.send_keys("adevratna@gmail.com") #_2AkmmA _1LctnI _7UHT_c
+loginId.send_keys("xxx@gmail.com") #_2AkmmA _1LctnI _7UHT_c
 
 password = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, "//*[@class='_2zrpKA _3v41xv']")));
-password.send_keys("dev1994arya")
+password.send_keys("xxxx")
 
 driver.find_element_by_xpath("//*[@class='_2AkmmA _1LctnI _7UHT_c']").click()
 
